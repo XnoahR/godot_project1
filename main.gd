1a02eea58747ab01a4d7bc697f21e06f0351b327
Rio Saputro <rio.ae23@gmail.com>
2023-12-30 14:16:01 +0700

    Input node to variable

--- main.gd
+++ main.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-
+var character
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	character = $cat
+	var myCatPosX = get_node("cat").position.x
 	get_node("cat2").position = Vector2(300,250)
-	$cat.scale.x = 2.5
+	$cat.scale.x = 2.5 #alternate use of get_node
+	print(myCatPosX)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	character.position.x += 125*delta
 	pass
--- scripts/cat.gd
+++ scripts/cat.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-
+var num = 2
+var stg : String = "Joe"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("inside _ready")
-
+	print(typeof(num))
+	print(typeof(stg))
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	position.x = position.x + 1
-	print(position.x)
-
+	pass
+	#position.x = position.x + (10*delta)
+	#print(position.x)
